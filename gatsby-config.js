4745ba3a8b5a40d87d0be91a313a7b39018eda4c
Nik Schaefer <nikkschaefer@gmail.com>
2021-01-01 00:36:00 -0600

    Meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Nik Schaefer',
     description:
-      'Brittany Chiang is a software engineer specializing in building (and occasionally designing) exceptional websites, applications, and everything in between.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
-    image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+      'I am a self- taught full- stack developer and hobbyist programmer student specializing in React and Django',
+    siteUrl: 'https://nikschaefer.tech/', // No trailing slash allowed!
+    image: '/me.png', // Path to your image you placed in the 'static' folder
+    twitterUsername: '@NikSchaefer_',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -19,8 +19,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'Nik Schaefer',
+        short_name: 'NikSchaefer',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
