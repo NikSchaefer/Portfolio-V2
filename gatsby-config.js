fb56c5eef1955efe80261265716dadd0404f6b99
Nik Schaefer <nikkschaefer@gmail.com>
2021-01-04 12:00:21 -0600

    Script

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,6 @@ module.exports = {
     url: 'https://nikschaefer.tech',
     language: `en-us`,
     keywords: ['Nik', 'Schaefer', 'Programming', 'Engineer', 'Software', 'Code'],
-
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -23,10 +22,27 @@ module.exports = {
     `gatsby-plugin-sitemap`,
     `gatsby-plugin-robots-txt`,
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: 'G-2B47V4H829',
-        head: true,
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'G-2B47V4H829', // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: false,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: [],
+        },
       },
     },
     {
@@ -154,6 +170,5 @@ module.exports = {
         ],
       },
     },
-    
   ],
 };
